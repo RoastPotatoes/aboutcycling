848c1c2fc9bc20a1ef355c191dd664066660f0ee
RoastPotatoes <atiorche@mailbox.org>
2024-11-02 12:53:00 +0000

    e

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "roastpotatoes.github.io"
+  site: "roastpotatoes.github.io",
   base: "aboutcycling",
   integrations: [mdx(), sitemap(), tailwind()],
 });
